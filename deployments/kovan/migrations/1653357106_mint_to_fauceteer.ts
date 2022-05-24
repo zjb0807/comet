19b8ba23605f1d435c0f2eb24fd52736c99d4941
Scott Silver <scott@compound.finance>
2022-05-23 19:22:02 -0700

    WBTC

--- deployments/kovan/migrations/1653357106_mint_to_fauceteer.ts
+++ deployments/kovan/migrations/1653357106_mint_to_fauceteer.ts
@@ -9,24 +9,24 @@ migration<Vars>('1653357106_mint_to_fauceteer', {
     const [signer] = await deploymentManager.hre.ethers.getSigners();
     const signerAddress = await signer.getAddress();
 
-    console.log(`signerAddress: ${signerAddress}`);
+    console.log(`Minting as signer: ${signerAddress}`);
 
     const contracts = await deploymentManager.contracts();
     const USDC = contracts.get('USDC');
     const fauceteer = contracts.get('fauceteer');
     const fauceteerAddress = fauceteer.address;
 
-    console.log(`USDC.address: ${USDC.address}`);
-    console.log(`fauceteer.address: ${fauceteer.address}`);
-    console.log(`await USDC.masterMinter(): ${await USDC.masterMinter()}`);
-
+    // USDC
     console.log(`minting USDC@${USDC.address} to fauceteer@${fauceteerAddress}`);
-
     await wait(USDC.configureMinter(signerAddress, BigInt(100_000_000 * 1e6))); // mint 100M USDC
     await wait(USDC.mint(fauceteerAddress, BigInt(100_000_000 * 1e6)));
-
     console.log(`USDC.balanceOf(fauceteerAddress): ${await USDC.balanceOf(fauceteerAddress)}`);
 
+    // WBTC
+    const WBTC = contracts.get('WBTC');
+    console.log(`minting WBTC@${WBTC.address} to fauceteer${fauceteerAddress}`);
+    await wait(WBTC.mint(fauceteerAddress, exp(100_000_000, 8))); // 100M WBTC
+    console.log(`WBTC.balanceOf(fauceteerAddress): ${await WBTC.balanceOf(fauceteerAddress)}`);
 
     return {};
   },
