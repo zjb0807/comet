97d653602970472d48349fa155090621910b7098
Scott Silver <scott@compound.finance>
2022-05-23 19:45:34 -0700

    COMP

--- deployments/kovan/migrations/1653357106_mint_to_fauceteer.ts
+++ deployments/kovan/migrations/1653357106_mint_to_fauceteer.ts
@@ -12,21 +12,28 @@ migration<Vars>('1653357106_mint_to_fauceteer', {
     console.log(`Minting as signer: ${signerAddress}`);
 
     const contracts = await deploymentManager.contracts();
-    const USDC = contracts.get('USDC');
     const fauceteer = contracts.get('fauceteer');
     const fauceteerAddress = fauceteer.address;
 
-    // USDC
+    // USDC (6 decimals)
+    const USDC = contracts.get('USDC');
     console.log(`minting USDC@${USDC.address} to fauceteer@${fauceteerAddress}`);
     await wait(USDC.configureMinter(signerAddress, BigInt(100_000_000 * 1e6))); // mint 100M USDC
     await wait(USDC.mint(fauceteerAddress, BigInt(100_000_000 * 1e6)));
     console.log(`USDC.balanceOf(fauceteerAddress): ${await USDC.balanceOf(fauceteerAddress)}`);
 
-    // WBTC
+    // WBTC (8 decimals)
     const WBTC = contracts.get('WBTC');
     console.log(`minting WBTC@${WBTC.address} to fauceteer${fauceteerAddress}`);
     await wait(WBTC.mint(fauceteerAddress, exp(100_000_000, 8))); // 100M WBTC
     console.log(`WBTC.balanceOf(fauceteerAddress): ${await WBTC.balanceOf(fauceteerAddress)}`);
+
+    // COMP (18 decimals)
+    const COMP = contracts.get('COMP');
+    const signerCompBalance = await COMP.balanceOf(signerAddress);
+    console.log(`minting COMP@${COMP.address} to fauceteer@${fauceteerAddress}`);
+    await COMP.transfer(fauceteerAddress, signerCompBalance.div(2)); // transfer half of signer's balance
+    console.log(`COMP.balanceOf(fauceteerAddress): ${await COMP.balanceOf(fauceteerAddress)}`);
 
     return {};
   },
