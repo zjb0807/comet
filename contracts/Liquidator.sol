4cfacc9c3e1db75e7645a2d9d7ba018605f1df99
Scott Silver <scott@compound.finance>
2022-06-24 16:27:19 -0700

    light clean-up

--- contracts/Liquidator.sol
+++ contracts/Liquidator.sol
@@ -1,14 +1,14 @@
+// XXX import CometInterface
 
 contract Liquidator {
     /** Custom errors **/
+
+    /** Constants **/
     address public immutable comet;
 
     constructor(address cometAddress, address _absorber) {
-        // contract admin? pause guardian?
         comet = CometInterface(cometAddress);
-        absorber = absorber;
-
-        // add some dust
+        absorber = _absorber;
     }
 
     // routesForCollateralAsset = collateral->ETH->base or collateral->base
@@ -17,7 +17,7 @@ contract Liquidator {
     // path = []
     // paths[]
 
-    function liquidate(address[] calldata accounts, collateralAssetsToBuy[], routesForCollateralAssets[route[]]) {
+    function liquidate(address[] calldata accounts, collateralAssetsToBuy[], routesForCollateralAssets[route[]]) public {
         // Absorb the underwater addresses
         comet.absorb(liquidator, accounts);
 
@@ -41,25 +41,27 @@ contract Liquidator {
             // get the reverse price ->
             // 1 / quotecollateral(assetaddress, 1 base or something) = base units per unit of collateral
 
+            // quoteCollateral(address asset, uint baseAmount) override public view returns (uint)
+
             // loanAmount += amount
 
             // buyCalls.push([]) // uniswap buy cal format (minAmount, pairAddress, route,...)
-                // buy (collateral balance * the per unit of collateral price)
 
+            // buy (collateral balance * the per unit of collateral price)
         }
 
         // take out the flash loan for loanAmount
-        profit = 0
+        profit = 0;
 
-        for (uint8 i =0; i < buyCalls.length; i++ ) {
+        for (uint8 i = 0; i < buyCalls.length; i++) {
             // buyCollateral(asset, collateralBalance, baseUnitsPerCollateralUnit * collateralBalance, msg.sender)
 
-            // buyCollateral(...)
+            // buyCollateral(address asset, uint minAmount, uint baseAmount, address recipient) override external {
 
             // sell it (pass route for this asset to uniswap)
         }
 
-        // figure out how much debt to reapy ont he flash loan
+        // figure out how much debt to repay on the flash loan
         // repay it
 
         // Revert if this wasn't profitable
